82c36b8306aa9d00689efca01cd6e0a0e4d2eb60
Lance Niles <lniles@spotxchange.com>
2015-05-21 13:19:25 -0600

    Destroy SpotXView when ad is complete
    Change-Id: I9435a6b04ae7494dcba8cce7411ec95098f5b998
    JIRA: YWS-2699

--- app/src/main/java/com/spotxchange/demo/InterstitialViewFragment.java
+++ app/src/main/java/com/spotxchange/demo/InterstitialViewFragment.java
@@ -19,7 +19,7 @@ import com.spotxchange.integration.mraid.utils.VpaidEventListener;
 
 public class InterstitialViewFragment extends Fragment implements View.OnClickListener {
 
-    private static final String LOGTAG = "InterstitlViewFragment";
+    private static final String LOGTAG = InterstitialViewFragment.class.getSimpleName();
 
     RelativeLayout mLayoutMain;
     SpotXView mSpotXView;
@@ -28,6 +28,12 @@ public class InterstitialViewFragment extends Fragment implements View.OnClickLi
         @Override
         public void onVpaidEvent(VpaidEvent vpaidEvent) {
             Log.d(LOGTAG, "onVpaidEvent: " + vpaidEvent);
+
+            switch (vpaidEvent) {
+                case AD_VIDEO_COMPLETE:
+                case AD_ERROR:
+                    destroyInterstitialView();
+            }
         }
     };
 
@@ -56,10 +62,8 @@ public class InterstitialViewFragment extends Fragment implements View.OnClickLi
 
     @Override
     public void onDestroyView() {
+        destroyInterstitialView();
         super.onDestroyView();
-        if (mSpotXView != null) {
-            mSpotXView.close();
-        }
     }
 
     public int getChannelIdFromEditText() {
@@ -74,30 +78,38 @@ public class InterstitialViewFragment extends Fragment implements View.OnClickLi
         return result;
     }
 
-    public void launchInterstitialView() {
+    private void launchInterstitialView() {
+        destroyInterstitialView();
+
         int channelId = getChannelIdFromEditText();
 
         // Create an INTERSTITIAL SpotX Ad
-        mSpotXView = new SpotXView(getActivity(), PlacementType.INTERSTITIAL);
-        SpotXProperties properties = new SpotXProperties();
-        properties.put(SpotXProperties.APP_STORE_URL,
-                "https://play.google.com/store/apps/details?id=com.example.app");
-        properties.put(SpotXProperties.APP_IAB_CATEGORY, "IAB1");
-        // R.id.programmatic_ad set in the ids.xml values file
-        mSpotXView.initialize(R.id.programmatic_ad, channelId, "com.app.publisher.domain", properties);
 
-        // Listen for VPAID 2.0 Events
+        SpotXProperties properties = new SpotXProperties();
+        properties.put(SpotXProperties.APP_IAB_CATEGORY, "IAB1");
+        properties.put(SpotXProperties.APP_STORE_URL, "https://play.google.com/store/apps/details?id=com.example.app");
+
+        // R.id.programmatic_ad set in the ids.xml values file
+        mSpotXView = new SpotXView(getActivity(), PlacementType.INTERSTITIAL);
+        mSpotXView.initialize(R.id.programmatic_ad, channelId, "com.app.publisher.domain", properties);
         mSpotXView.setVpaidEventListener(mVpaidEventListener);
 
         // Relative layout params
         RelativeLayout.LayoutParams adLayoutParams =
                 new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
-        // Try/Catch because it's not uncommon to create fragments without views.
-        try {
-            ((FrameLayout)(getView().getRootView().findViewById(android.R.id.content))).addView(mSpotXView, adLayoutParams);
-        } catch (Exception ignore) {
-            ignore.printStackTrace();
+        getRootContentView().addView(mSpotXView);
+    }
+
+    private void destroyInterstitialView() {
+        if (mSpotXView != null) {
+            getRootContentView().removeView(mSpotXView);
+            mSpotXView.close();
+            mSpotXView = null;
         }
+    }
+
+    private ViewGroup getRootContentView() {
+        return (ViewGroup) getView().getRootView().findViewById(android.R.id.content);
     }
 }
