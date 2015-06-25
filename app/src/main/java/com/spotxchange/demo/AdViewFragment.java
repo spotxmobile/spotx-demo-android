3b338cee5fa6ed7d8a6780df6e6aed44f295e3e1
Patrick <pmitchell@spotxchange.com>
2015-06-25 16:04:50 -0600

    Demo :: SpotxAdListener
    Added a listener for precache-style autoplay. The listener
    will make the ad visible when it's loaded.
    
    Change-Id: I7f739d5320d86f62064d0e6d294473bad163e814
    JIRA: YWS-2429

--- app/src/main/java/com/spotxchange/demo/AdViewFragment.java
+++ app/src/main/java/com/spotxchange/demo/AdViewFragment.java
@@ -6,9 +6,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.RelativeLayout;
 
+import com.spotxchange.sdk.android.SpotxAdListener;
 import com.spotxchange.sdk.android.SpotxAdView;
 
 
@@ -16,16 +16,8 @@ public class AdViewFragment extends Fragment {
 
     private final static String LOGTAG = "XmlFragment";
 
-    private RelativeLayout mLayoutMain;
-    private SpotxAdView mSpotXView;
-
-    /*
-    VpaidEventListener mVpaidEventListener = new VpaidEventListener() {
-        @Override
-        public void onVpaidEvent(VpaidEvent vpaidEvent) {
-            Log.d(LOGTAG, "onVpaidEvent: " + vpaidEvent);
-        }
-    };*/
+    private RelativeLayout _layout;
+    private SpotxAdView _adView;
 
     public AdViewFragment() {
         // Required empty public constructor
@@ -36,40 +28,70 @@ public class AdViewFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        mLayoutMain = (RelativeLayout)inflater.inflate(R.layout.fragment_adview, container, false);
+        _layout = (RelativeLayout)inflater.inflate(R.layout.fragment_adview, container, false);
 
-        //((EditText)mLayoutMain.findViewById(R.id.edittext_xml_channel_id)).setHint(MainActivity.DEFAULT_CHANNEL_ID_HINT);
+        //((EditText)_layout.findViewById(R.id.edittext_xml_channel_id)).setHint(MainActivity.DEFAULT_CHANNEL_ID_HINT);
 
         // Get the channel id from the edit text
         int channelId = getChannelIdFromEditText();
 
         // Create the SpotX Ad
-        mSpotXView = (SpotxAdView)mLayoutMain.findViewById(R.id.adview_example);
-
-        // Listen for VPAID 2.0 Events
-        //mSpotXView.setVpaidEventListener(mVpaidEventListener);
+        _adView = (SpotxAdView) _layout.findViewById(R.id.adview_example);
 
         // Register button for channel id editing
         /*
-        mLayoutMain.findViewById(R.id.button_change_xml_channel).setOnClickListener(new View.OnClickListener() {
+        _layout.findViewById(R.id.button_change_xml_channel).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
                     int channelId = getChannelIdFromEditText();
-                    mSpotXView.loadAdWithNewId(channelId);
+                    _adView.loadAdWithNewId(channelId);
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
         });*/
 
-        return mLayoutMain;
+        SpotxAdListener adListener = new SpotxAdListener() {
+            @Override
+            public void adLoaded() {
+                Log.d(LOGTAG, "Ad Was loaded.");
+                _adView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void adStarted() {
+                Log.d(LOGTAG, "Ad Was started.");
+            }
+
+            @Override
+            public void adCompleted() {
+                Log.d(LOGTAG, "Ad has completed.");
+                _adView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void adError() {
+                Log.d(LOGTAG, "There was an ad error");
+                _adView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void adExpired() {
+                Log.d(LOGTAG, "Ad has expired");
+                //TODO: AdViewFragment.createNewView();
+            }
+        };
+
+        _adView.setAdListener(adListener);
+
+        return _layout;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        mSpotXView = null;
+        _adView = null;
     }
 
     public int getChannelIdFromEditText() {
@@ -77,7 +99,7 @@ public class AdViewFragment extends Fragment {
         /*
         int result = -1;
         try {
-            EditText textBox = ((EditText) mLayoutMain.findViewById(R.id.edittext_xml_channel_id));
+            EditText textBox = ((EditText) _layout.findViewById(R.id.edittext_xml_channel_id));
             String channelIdString = (textBox.getText().toString().isEmpty() || textBox.getText() == null) ? textBox.getHint().toString() : textBox.getText().toString();
             result = Integer.parseInt(channelIdString);
         } catch (Exception ignore) {
